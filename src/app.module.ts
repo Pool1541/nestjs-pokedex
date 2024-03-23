d89fb6118a8d71374a4be19ae8150a76315aee2e
pool_1541 <pool_1541@hotmail.com>
2024-03-22 21:41:41 -0500

    feat(module): update app.module

--- src/app.module.ts
+++ src/app.module.ts
@@ -20,7 +20,9 @@ import { JoiValidationSchema } from './config/joi.valitation';
       rootPath: join(__dirname, '..', 'public'),
     }),
 
-    MongooseModule.forRoot(process.env.MONGODB),
+    MongooseModule.forRoot(process.env.MONGODB, {
+      dbName: 'pokemondb',
+    }),
 
     PokemonModule,
 
